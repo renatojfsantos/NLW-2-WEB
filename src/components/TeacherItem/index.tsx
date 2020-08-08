3260492538f0ee50f61f41f321a5c0a480ce1c23
Renato Santos <renato_jfs@hotmail.com>
2020-08-08 01:58:55 -0300

    Web completo

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,35 +1,55 @@
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
 import './styles.css';
 
-function TeacherItem () {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars1.githubusercontent.com/u/67006002?s=460&u=9a2ccd94aa661b28bc8cb1a2e6da0217f30b7dad&v=4" alt="Renato Santos"/>
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Renato Santos</strong>
-                    <span>React e React-native</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Entusiasta de novas tecnologias
-                <br /><br />
-                Cada dia um novo aprendizado sobre novas tecnologias e assim sempre evoluindo para tornar o mundo melhor. Então sempre codando e aprendendo!!!
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$200,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a
+                    target="_blank"
+                    onClick={createNewConnection} 
+                    href={`http://wa.me/${teacher.whatsapp}`}
+                >
                     <img src={whatsappIcon} alt="whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
